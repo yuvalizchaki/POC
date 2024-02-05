c8ee88b2fccd71302fb0ddb24da979005a4bc45f
Taltalonix <tal@mry.biz>
2024-02-05 23:40:20 +0200

    Mock server uses real storage

--- mock-server/src/app.ts
+++ mock-server/src/app.ts
@@ -1,21 +1,26 @@
-import express from 'express';
-import ordersRouter from './routes/orders.routes';
-import path from 'path';
+import express from "express";
+import ordersRouter from "./routes/orders.routes";
+import path from "path";
 
 const app = express();
 const port = 8008;
 
+// Middleware to avoid caching
+app.use((req, res, next) => {
+    res.setHeader("Cache-Control", "no-store");
+    next();
+  });
 app.use(express.json());
 app.use(ordersRouter);
 
 // Serve static files from the "public" directory
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Specific route for gui.html
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'gui.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "gui.html"));
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
--- mock-server/src/services/orders.service.ts
+++ mock-server/src/services/orders.service.ts
@@ -2,31 +2,49 @@ import { OrderDto } from "../types/order.dto";
 import _mockOrders from "../data/mockOrders.json";
 
 /** Type Fix */
-const orders = _mockOrders as unknown as OrderDto[];
+const mockOrders = _mockOrders as unknown as OrderDto[];
+const orders: OrderDto[] = [];
 
 export const getAllOrders = (count?: number): OrderDto[] => {
-    return count ? orders.slice(0, count) : orders;
+  return count ? orders.slice(0, count) : orders;
 };
 
 export const findOrderById = (orderId: string): OrderDto | undefined => {
-    return orders.find(order => order.Id.toString() === orderId);
+  return orders.find((order) => order.Id.toString() === orderId);
 };
 
 export const addOrder = (): OrderDto => {
-    const newOrder = orders[Math.floor(Math.random() * orders.length)];
-    return newOrder; // Simulate adding an order
+  let newOrder: OrderDto;
+  do {
+    newOrder = mockOrders[Math.floor(Math.random() * mockOrders.length)];
+  } while (orders.find((order) => order.Id === newOrder.Id));
+
+  orders.push(newOrder);
+  return newOrder; // Simulate adding a non-duplicate order
 };
 
-export const updateOrder = (orderId: string, updateData: Partial<OrderDto>): OrderDto | undefined => {
-    const orderIndex = orders.findIndex(order => order.Id.toString() === orderId);
-    if (orderIndex !== -1) {
-        const updatedOrder = {...orders[orderIndex], ...updateData};
-        return updatedOrder; // Simulate updating an order
-    }
-    return undefined;
+export const updateOrder = (
+  orderId: string,
+  updateData: Partial<OrderDto>
+): OrderDto | undefined => {
+  const orderIndex = orders.findIndex(
+    (order) => order.Id.toString() === orderId
+  );
+  if (orderIndex !== -1) {
+    const updatedOrder = { ...orders[orderIndex], ...updateData };
+    orders[orderIndex] = updatedOrder;
+    return updatedOrder; // Simulate updating an order
+  }
+  return undefined;
 };
 
 export const deleteOrder = (orderId: string): boolean => {
-    const orderIndex = orders.findIndex(order => order.Id.toString() === orderId);
-    return orderIndex !== -1; // Simulate deleting an order
+  const orderIndex = orders.findIndex(
+    (order) => order.Id.toString() === orderId
+  );
+  if (orderIndex !== -1) {
+    orders.splice(orderIndex, 1);
+    return true;
+  }
+  return false;
 };
