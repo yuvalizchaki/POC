a8af743f48885e8a5573dffa0e00724716f3a0c8
Yuval izchaki <you@example.com>
2024-05-08 14:33:02 +0300

    Clean the code by dividing it into components.

---
+++ frontend/poc-fe/src/pages/admin/dashboard/components/addScreenProfileComponent.tsx
@@ -0,0 +1,91 @@
+import { Box, Button, Grid, Modal, TextField, Typography } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import { createScreenProfile } from "../../../../services/adminService";
+import { useState, ChangeEvent } from "react";
+
+interface AddScreenProfileProps {
+  style: any;
+  fetchScreenProfiles: () => void;
+}
+
+export const AddScreenProfileComponent = ({
+  style,
+  fetchScreenProfiles,
+}: AddScreenProfileProps) => {
+  const [addScreenProfileOpen, setAddScreenProfileOpen] = useState(false);
+  const [name, setName] = useState("");
+
+  const handleAddScreenProfileOpen = () => {
+    setAddScreenProfileOpen(true);
+  };
+
+  const handleAddScreenProfileClose = () => {
+    setAddScreenProfileOpen(false);
+  };
+
+  const handleAddScreenProfile = () => {
+    createScreenProfile(name).then(() => {
+      fetchScreenProfiles();
+    });
+    setAddScreenProfileOpen(false);
+  };
+
+  const handleNameChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setName(event.target.value);
+  };
+
+  return (
+    <Box display="flex" alignItems="center">
+      <Typography variant="h5">Screen Profiles</Typography>
+      <div style={{ flex: "1 0 0" }} />
+      <Button
+        variant="outlined"
+        startIcon={<AddIcon />}
+        onClick={handleAddScreenProfileOpen}
+      >
+        Add Profile
+      </Button>
+      <Modal
+        open={addScreenProfileOpen}
+        onClose={handleAddScreenProfileClose}
+        aria-labelledby="parent-modal-title"
+        aria-describedby="parent-modal-description"
+      >
+        <Box sx={{ ...style, width: 400 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                label="Name"
+                value={name}
+                onChange={handleNameChange} // Call the function to update the 'name' state
+              />
+            </Grid>
+            <Grid item xs={6} sx={{ display: "flex" }}>
+              <div style={{ flex: "1 0 0" }} />
+              <Button
+                disableElevation
+                color="error"
+                variant="text"
+                onClick={handleAddScreenProfileClose}
+              >
+                Cancel
+              </Button>
+              <Button
+                disableElevation
+                variant="contained"
+                sx={{ ml: 2 }}
+                onClick={handleAddScreenProfile}
+              >
+                Save
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Modal>
+    </Box>
+  );
+};
--- frontend/poc-fe/src/pages/admin/dashboard/screen-management.page.tsx
+++ frontend/poc-fe/src/pages/admin/dashboard/screen-management.page.tsx
@@ -1,31 +1,10 @@
-import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  Box,
-  Button,
-  Divider,
-  Grid,
-  IconButton,
-  Modal,
-  Stack,
-  TextField,
-  Typography,
-} from "@mui/material";
+import { Stack } from "@mui/material";
 import { ScreenProfile } from "../../../types/screenProfile.types";
+import { getAllScreenProfiles } from "../../../services/adminService";
+import { useState } from "react";
+import { AddScreenProfileComponent } from "./components/addScreenProfileComponent";
+import { ProfileComponent } from "./components/profileComponent";
 
-import AddIcon from "@mui/icons-material/Add";
-// import EditIcon from "@mui/icons-material/Edit";
-import DeleteIcon from "@mui/icons-material/Delete";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import {
-  createScreenProfile,
-  deleteScreenProfile,
-  getAllScreenProfiles,
-  pairScreen,
-} from "../../../services/adminService";
-import { ScreenComponent } from "./components/screenComponent";
-import React, { useState, useEffect, ChangeEvent } from "react";
 const style = {
   position: "absolute",
   top: "50%",
@@ -39,6 +18,7 @@ const style = {
   px: 4,
   pb: 3,
 };
+
 function AdminDashboard() {
   const [profiles, setProfiles] = useState<ScreenProfile[]>([]);
   const fetchScreenProfiles = () => {
@@ -46,204 +26,19 @@ function AdminDashboard() {
       setProfiles(data);
     });
   };
-
-  const [addScreenProfileOpen, setAddScreenProfileOpen] = useState(false);
-  const [addScreenOpen, setAddScreenOpen] = useState(false);
-
-  const [name, setName] = useState("");
-  const handleAddScreenProfileOpen = () => {
-    setAddScreenProfileOpen(true);
-  };
-  const handleAddScreenProfileClose = () => {
-    setAddScreenProfileOpen(false);
-  };
-  const handleAddScreenProfile = () => {
-    createScreenProfile(name).then(() => {
-      fetchScreenProfiles();
-    });
-    setAddScreenProfileOpen(false);
-  };
-  const handleNameChange = (
-    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    setName(event.target.value);
-  };
-
-  const [code, setCode] = useState("");
-  const handleCodeChange = (
-    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    setCode(event.target.value);
-  };
-  const handleAddScreenOpen = () => {
-    setAddScreenOpen(true);
-  };
-  const handleAddScreenClose = () => {
-    setAddScreenOpen(false);
-  };
-
-  const handleAddScreen = (id: number) => {
-    pairScreen(code, id).then(() => {
-      fetchScreenProfiles();
-    });
-    setAddScreenOpen(false);
-  };
-  const handleDeleteScreenProfile = (id: number) => {
-    deleteScreenProfile(id).then(() => {
-      fetchScreenProfiles();
-    });
-  };
-  useEffect(() => {
-    fetchScreenProfiles();
-  }, []);
   return (
     <Stack direction="column" spacing={2}>
-      <Box display="flex" alignItems="center">
-        <Typography variant="h5">Screen Profiles</Typography>
-        <div style={{ flex: "1 0 0" }} />
-        <Button
-          variant="outlined"
-          startIcon={<AddIcon />}
-          onClick={handleAddScreenProfileOpen}
-        >
-          Add Profile
-        </Button>
-        <Modal
-          open={addScreenProfileOpen}
-          onClose={handleAddScreenProfileClose}
-          aria-labelledby="parent-modal-title"
-          aria-describedby="parent-modal-description"
-        >
-          <Box sx={{ ...style, width: 400 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={6}>
-                <TextField
-                  variant="outlined"
-                  fullWidth
-                  label="Name"
-                  value={name}
-                  onChange={handleNameChange} // Call the function to update the 'name' state
-                />
-              </Grid>
-              <Grid item xs={6} sx={{ display: "flex" }}>
-                <div style={{ flex: "1 0 0" }} />
-                <Button
-                  disableElevation
-                  color="error"
-                  variant="text"
-                  onClick={handleAddScreenProfileClose}
-                >
-                  Cancel
-                </Button>
-                <Button
-                  disableElevation
-                  variant="contained"
-                  sx={{ ml: 2 }}
-                  onClick={handleAddScreenProfile}
-                >
-                  Save
-                </Button>
-              </Grid>
-            </Grid>
-          </Box>
-        </Modal>
-      </Box>
+      <AddScreenProfileComponent
+        style={style}
+        fetchScreenProfiles={fetchScreenProfiles}
+      />
       {profiles.map((p) => {
         return (
-          <React.Fragment key={p.id}>
-            <Accordion
-              variant="outlined"
-              sx={{ borderRadius: 2 }}
-              onClick={(e: React.MouseEvent<HTMLDivElement>) => {
-                console.log("[DEBUG] e: ", e);
-                if (e.target !== e.currentTarget) {
-                  e.preventDefault();
-                }
-              }}
-            >
-              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                <Stack direction="row" alignItems="center" width="100%">
-                  <Typography>{p.name}</Typography>
-                  <div style={{ flex: "1 0 0" }} />
-                  <Button
-                    size="small"
-                    variant="text"
-                    startIcon={<AddIcon />}
-                    onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
-                      e.stopPropagation();
-                      handleAddScreenOpen();
-                    }}
-                  >
-                    Add Screen
-                  </Button>
-                  <IconButton
-                    color="error"
-                    size="small"
-                    onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
-                      e.stopPropagation();
-                      handleDeleteScreenProfile(p.id);
-                    }}
-                  >
-                    <DeleteIcon />
-                  </IconButton>
-                </Stack>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Stack direction="column" spacing={1}>
-                  {p.screens.map((s, i) => (
-                    <React.Fragment key={s.id}>
-                      {i > 0 && <Divider />}
-                      <ScreenComponent
-                        screen={s}
-                        fetchScreenProfiles={fetchScreenProfiles}
-                      />
-                    </React.Fragment>
-                  ))}
-                </Stack>
-              </AccordionDetails>
-            </Accordion>
-            <Modal
-                    open={addScreenOpen}
-                    onClose={handleAddScreenClose}
-                    aria-labelledby="parent-modal-title"
-                    aria-describedby="parent-modal-description"
-                  >
-                    <Box sx={{ ...style, width: 400 }}>
-                      <Grid container spacing={2}>
-                        <Grid item xs={6}>
-                          <TextField
-                            variant="outlined"
-                            fullWidth
-                            label="code"
-                            value={code}
-                            onChange={handleCodeChange} // Call the function to update the 'code' state
-                          />
-                        </Grid>
-                        <Grid item xs={6} sx={{ display: "flex" }}>
-                          <div style={{ flex: "1 0 0" }} />
-                          <Button
-                            disableElevation
-                            color="error"
-                            variant="text"
-                            onClick={handleAddScreenClose}
-                          >
-                            Cancel
-                          </Button>
-                          <Button
-                            disableElevation
-                            variant="contained"
-                            sx={{ ml: 2 }}
-                            onClick={() => {
-                              handleAddScreen(p.id);
-                            }}
-                          >
-                            Save
-                          </Button>
-                        </Grid>
-                      </Grid>
-                    </Box>
-                  </Modal>
-          </React.Fragment>
+          <ProfileComponent
+            style={style}
+            profile={p}
+            fetchScreenProfiles={fetchScreenProfiles}
+          />
         );
       })}
     </Stack>
