021f9e15239a204b121472cbccda8016312be55f
Taltalonix <tal@mry.biz>
2024-05-27 22:57:39 +0300

    added useScreenTemplate hook

--- frontend/poc-fe/src/components/screen-templates/orders-screen/orders-screen.component.tsx
+++ frontend/poc-fe/src/components/screen-templates/orders-screen/orders-screen.component.tsx
@@ -1,90 +1,15 @@
-import { OrderDto } from "../../../types/crmTypes.types";
-import { useCallback, useState } from "react";
-import { useSignalR } from "../../../hooks/useSignalR";
-import { API_BASE_URL, API_SCREEN_HUB_URL } from "../../../config";
-import { ScreenInfo } from "../../../types/screenInfo.types";
 import { Box, Divider, Paper, Typography } from "@mui/material";
-import { useScreenInfo } from "../../../hooks/useScreenInfo";
+import { OrderDto } from "../../../types/crmTypes.types";
+import { ScreenInfo } from "../../../types/screenInfo.types";
+import { useScreenTemplate } from "../../../hooks/useScreenTemplate";
 
 interface OrdersScreenProps {
   screenInfo: ScreenInfo;
 }
 
 export const OrdersScreen = ({ screenInfo }: OrdersScreenProps) => {
-  const { setScreenInfo } = useScreenInfo();
+  const { orders } = useScreenTemplate({ screenInfo });
 
-  const [orders, setOrders] = useState<OrderDto[]>([]);
-
-  const fetchOrders = useCallback(async () => {
-    try {
-      console.log("[DEBUG] sending: ", `${API_BASE_URL}/orders`);
-      const response = await fetch(`${API_BASE_URL}/orders`);
-      console.log("[DEBUG] get orders response: ", response);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-      setOrders(data);
-    } catch (error) {
-      console.error("Failed to fetch orders:", error);
-    }
-  }, []);
-
-  useSignalR({
-    hubUrl: API_SCREEN_HUB_URL,
-    onConnect: () => {
-      console.log("[DEBUG] connected to screen hub");
-      fetchOrders();
-    },
-    commandHandlers: {
-      orderAdded: (orderAddedDto) => {
-        setOrders((prevOrders) => {
-          if (prevOrders.some((order) => order.id === orderAddedDto.id)) {
-            console.warn(
-              "Attempted to add an existing order. Fetching latest orders."
-            );
-            fetchOrders();
-            return prevOrders;
-          }
-          return [...prevOrders, orderAddedDto];
-        });
-      },
-      orderUpdated: (orderUpdatedDto) => {
-        setOrders((prevOrders) => {
-          const orderIndex = prevOrders.findIndex(
-            (order) => order.id === orderUpdatedDto.id
-          );
-          if (orderIndex === -1) {
-            console.warn(
-              "Attempted to update a non-existing order. Fetching latest orders."
-            );
-            fetchOrders();
-            return prevOrders;
-          }
-          const updatedOrders = [...prevOrders];
-          updatedOrders[orderIndex] = orderUpdatedDto;
-          return updatedOrders;
-        });
-      },
-      orderDeleted: (orderDeletedDto) => {
-        setOrders((prevOrders) => {
-          if (!prevOrders.some((order) => order.id === orderDeletedDto)) {
-            console.warn(
-              "Attempted to delete a non-existing order. Fetching latest orders."
-            );
-            fetchOrders();
-            return prevOrders;
-          }
-          return prevOrders.filter((order) => order.id !== orderDeletedDto);
-        });
-      },
-      screenRemoved: (/* screenRemovedDto */) =>{
-        // TODO: Check this logic
-        console.log('[DEBUG] Disconnected')
-        setScreenInfo(null)
-      }
-    },
-  });
   return (
     <>
       <Paper variant="outlined" sx={{ p: 2, overflow: "auto" }}>
