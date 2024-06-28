553eaccbe4fbce91bd25efed22fc62d0ba57f8dc
oriKay <karato@post.bgu.ac.il>
2024-06-28 16:07:08 +0300

    update hub on screen and profile deletions to remove from the hub. hub sends now messages accordingly as well to the frontend to take care of the connection

--- backend/POC/POC/App/Commands/RemoveScreen/RemoveScreenCommandHandler.cs
+++ backend/POC/POC/App/Commands/RemoveScreen/RemoveScreenCommandHandler.cs
@@ -8,7 +8,7 @@ namespace POC.App.Commands.RemoveScreen;
 
 public class RemoveScreenCommandHandler(ScreenRepository repository,
     ILogger<RemoveScreenCommandHandler> logger,
-    //ScreenHub screenHub,
+    ScreenHub screenHub,
     ScreenProfileRepository screenProfileRepository //TODO DELETE THIS WHEN WE HAVE A PROPER DB THAT DOES THIS FOR US
     ) : IRequestHandler<RemoveScreenCommand>
 {
@@ -32,7 +32,7 @@ public class RemoveScreenCommandHandler(ScreenRepository repository,
 
             var screenDto = screen.ToScreenDto();
 
-            //await screenHub.RemoveScreen(screenDto); <-- Re-add when we implement the tokens
+            await screenHub.RemoveScreen(screenDto); //<-- Re-add when we implement the tokens
         }
         catch (Exception ex)
         {
@@ -43,41 +43,4 @@ public class RemoveScreenCommandHandler(ScreenRepository repository,
             throw;
         }
     }
-
-    // Code for whenever we have a database
-    //public async Task Handle(RemoveScreenCommand request, CancellationToken cancellationToken)
-    //{
-    //    try
-    //    {
-    //        var screen = await repository.GetByIdAsync(request.Id);
-    //        if (screen == null)
-    //        {
-    //            throw new ScreenNotFoundException();
-    //        }
-
-    //        using (var transaction = await database.BeginTransactionAsync())
-    //        {
-    //            // Assuming updateScreenDeleteAsync is idempotent and handles its own exceptions
-    //            await screenProfileRepository.updateScreenDeleteAsync(screen.Id, screen.ScreenProfileId);
-
-    //            var result = await repository.DeleteAsync(request.Id);
-    //            if (!result)
-    //            {
-    //                throw new Exception("Failed to delete screen.");
-    //            }
-
-    //            await screenHub.RemoveScreen(screen);
-
-    //            await transaction.CommitAsync();
-    //        }
-    //    }
-    //    catch (Exception ex)
-    //    {
-    //        // Log the exception
-    //        logger.LogError(ex, "Error occurred while handling RemoveScreenCommand for Screen ID {ScreenId}", request.Id);
-
-    //        // Rethrow or handle the exception as appropriate
-    //        throw;
-    //    }
-    //}
 }
