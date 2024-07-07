9208ef400db0f8f4e6a7e4883221f29e6a722b01
oriKay <karato@post.bgu.ac.il>
2024-07-07 15:54:00 +0300

    minor additings

--- backend/POC/POC/Contracts/ScreenProfile/CreateScreenProfileDto.cs
+++ backend/POC/POC/Contracts/ScreenProfile/CreateScreenProfileDto.cs
@@ -7,6 +7,7 @@ public class CreateScreenProfileDto
     // TODO: Implement CRM adapter and related classes
 
     [Required]
+    [StringLength(20, ErrorMessage = "Name length can't be more than 20.")]
     public string Name { get; set; }
     
     [Required]
